073a015c0e44fb226371a7f7e56d9d76cb9dd7fa
dev <qwer123456>
2018-03-19 16:49:21 +0800

    例子

--- changshi/aa-service.js
+++ changshi/aa-service.js
@@ -1,10 +1,16 @@
-
+const MG = require('./db.js');
+MG.OnConnected(function () {
+    Gyro = MG.db.Gyro;
+});
 exports.setup = function (server) {
 
     // List all pays filter by: userId, date, reason, page, details
     server.add_get('/test', function (req, res) {
-       console.log('12345')
-        return {data:{aa:1}}
+        return Gyro.insert({'videoId':'1234'}).exec().then(function () {
+            console.log('12345')
+            return {data:{aa:1}}
+        })
+
     });
 
 
